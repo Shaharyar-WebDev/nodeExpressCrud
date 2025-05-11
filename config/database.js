76b90bc574b63414fcce71f9786538acdc3d84c8
Shaharyar-WebDev <shery.codes@gmail.com>
2025-05-12 01:32:05 +0500

    fixed database config

--- config/database.js
+++ config/database.js
@@ -2,8 +2,14 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
 
- mongoose.connect('mongodb://127.0.0.1:27017/contact').then(()=>{
-console.log("DB connnected!");
+//  mongoose.connect('mongodb://127.0.0.1:27017/contact').then(()=>{
+// console.log("DB connnected!");
+// });
+
+mongoose.connect("mongodb+srv://ahmedshaharyar00:ahmedshaharyar00@cluster0.zcjpybb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+
+    console.log("DB connnected!");
+
 });
 
 }
